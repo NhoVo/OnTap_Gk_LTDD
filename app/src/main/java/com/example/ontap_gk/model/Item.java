c2f94fc63200e3db282bd98c367619ff1a606398
Admins <vothanhnho8005@gmail.com>
2022-03-21 09:00:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ontap_gk/model/Item.java
@@ -0,0 +1,47 @@
+package com.example.ontap_gk.model;
+
+public class Item {
+   private int id;
+   private int img;
+   private String names;
+   private String price;
+
+    public Item(int id, int img, String names, String price) {
+        this.id = id;
+        this.img = img;
+        this.names = names;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
